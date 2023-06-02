d980a623a84ea06d63f3898808ccc8bc93863478
void <dmpatel4773@gmail.com>
2023-06-02 14:35:09 +0200

    added gameover screen

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -6,19 +6,27 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D birdBody;
     public float jumpStrength;
+    public GameLogicScript logic;
+    private bool isGameOver = false;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        logic = GameObject.FindGameObjectWithTag("GameLogic").GetComponent<GameLogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow) == true)
+        if(Input.GetKeyDown(KeyCode.UpArrow) == true && isGameOver == false)
         {
             birdBody.velocity = Vector2.up * jumpStrength;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        isGameOver = true;
     }
 }
--- Assets/Scripts/GameLogicScript.cs
+++ Assets/Scripts/GameLogicScript.cs
@@ -2,16 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameLogicScript : MonoBehaviour
 {
     public int score = 0;
     public Text scoreText;
+    public GameObject gameOverScreen;
 
     [ContextMenu("Increase Score")]
     public void increaseScore()
     {
         score += 1;
         scoreText.text = "Score: " + score.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
     }
 }
